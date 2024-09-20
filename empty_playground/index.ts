4e173581755b4db962fb279cdabf712b8671994d
Rainer Stropek <rainer@timecockpit.com>
2024-09-20 12:55:12 +0200

    Saved in TypeScript Playground at 2024-09-20T10:55:12.247Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,29 +1,7 @@
 function setup() {
-    createCanvas(300, 300);
-    background("gold")
+    createCanvas(500, 500);
+    background("skyblue");
 }
 
-function mouseClicked() {
-    noStroke();
-    fill("skyblue");
-    circle(mouseX, mouseY, 10);
-
-    fill("skyblue");
-    noStroke();
-    rect(0, height - 20, width / 2, height);
-
-    fill("black");
-    text(`X: ${mouseX}, Y: ${mouseY}`, 5, height - 5);
-
+function draw() {
 }
-
-function mouseMoved() {
-    noStroke();
-    fill("lime");
-    circle (mouseX, mouseY, 10)
-    rect(200, height - 20, width / 2, height);
-
-    fill("black");
-    text(`X: ${mouseX}, Y: ${mouseY}`, 200, height - 5);
-}
-
