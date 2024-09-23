9c6769ba87f7804b225800ddd47a7f9c0f456372
Rainer Stropek <rainer@timecockpit.com>
2024-09-23 09:21:23 +0200

    Saved in TypeScript Playground at 2024-09-23T07:21:23.006Z

--- robot/index.ts
+++ robot/index.ts
@@ -8,4 +8,12 @@ function setup() {
     fill("black")
     circle(250, 400, 60)
     circle(450, 400, 60)
+
+    strokeWeight(3)
+    line(350, 150, 350, 300)
+
+    noStroke();
+    fill("white");
+    circle(350, 150, 50)
+
 }
