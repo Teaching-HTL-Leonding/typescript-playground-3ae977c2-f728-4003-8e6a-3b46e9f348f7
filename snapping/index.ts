7ea3f4597460d6787cf9d3e4bcd650feb1172a51
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 08:56:48 +0200

    Saved in TypeScript Playground at 2024-09-26T06:56:48.105Z

--- snapping/index.ts
+++ snapping/index.ts
@@ -7,15 +7,25 @@ function mouseMoved() {
   const snap = 40; // marker must snap every 40 pixels
   const markerSize = 10; // size of marker
 
-rect(mouseX, mouseY, 60, 2);
+  background("lightblue")
+
+  rect(mouseX, mouseY, 25, 2);
   strokeWeight(2);
-  fill("black")
+  stroke("white");
 
-  rect(mouseX, mouseY, 2, 60, 2 )
-  rect(mouseX, mouseY, 2, -60, 2 )
-  rect(mouseX, mouseY, -60, 2 )
+  text(`X: ${mouseX}, Y: ${mouseY}`, 200, -5);
+  strokeWeight(3)
+  stroke("with")
 
-  
 
-  
+  rect(mouseX, mouseY, 2, 25, 2)
+  rect(mouseX, mouseY, 2, -25, 2)
+  rect(mouseX, mouseY, -25, 2)
+
+
+
+
+
+
+
 }
