a29d6bacad9cb8532f7737e82b581640572b491e
Rainer Stropek <rainer@timecockpit.com>
2024-10-09 19:08:00 +0200

    Saved in TypeScript Playground at 2024-10-09T17:08:00.229Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -13,14 +13,16 @@ function setup() {
     rect(width * 2 / 3, 0, width, height * 0.75)
 }
 
-mouseClicked()
+function mouseClicked() {
+    let message: string
 
-let message: string
+    if (mouseX < width / 3) {
+        message = "Yellow"
+    } else if (mouseX < width / 3 * 2) {
+        message = "Green"
+    } else {
+        message = "Red"
+    }
+    fill("white")
 
-if (mouseX < width / 3) {
-    message = "Yellow"
-} else if (mouseX < width / 3 * 2) {
-    message = "Green"
-} else {
-    message = "Red"
 }
