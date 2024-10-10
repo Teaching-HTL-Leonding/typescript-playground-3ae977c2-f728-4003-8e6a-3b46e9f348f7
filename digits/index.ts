df239728afefd2bc0024fa2bffaa3a84fc7f6395
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 10:47:53 +0200

    Saved in TypeScript Playground at 2024-10-10T08:47:53.376Z

---
+++ digits/index.ts
@@ -0,0 +1,29 @@
+function setup() {
+
+  const num = Math.floor(random(0, 1_000_000));
+  createCanvas(1000, 600)
+  background("black")
+
+  textAlign(CENTER, CENTER);
+  fill("yellow");
+  noStroke();
+  text(num, width / 2, height - 20);
+
+  stroke("yellow")
+  noFill()
+  strokeWeight(2)
+  rect(width / 14, height / 3, width / 9, height / 3)
+  rect(width / 5, height /3, width / 9, height / 3)
+  rect(width /3, height /3, width / 9, height / 3)
+  rect(width /2.15, height /3, width / 9, height / 3)
+  rect(width /1.68, height /3, width / 9, height / 3)
+  rect(width /1.38, height /3, width /9, height /3)
+
+
+  
+  
+
+
+
+
+}
