6471b27822a719f45c16ed78e97a3e2178ecea31
Rainer Stropek <rainer@timecockpit.com>
2024-10-14 14:43:55 +0200

    Saved in TypeScript Playground at 2024-10-14T12:43:55.235Z

--- binary_digits/index.ts
+++ binary_digits/index.ts
@@ -2,6 +2,19 @@ function setup() {
 
   const num = Math.floor(random(0, 64));
   const lastDigit1 = num % 2
+  const numwithoutlastdigit1 = Math.floor(num / 2)
+  const lastDigit2 = numwithoutlastdigit1 % 2
+  const numwithoutlastdigit2 = Math.floor(numwithoutlastdigit1 / 2)
+  const lastDigit3 = numwithoutlastdigit2 % 2
+  const numwithoutlastdigit3 = Math.floor(numwithoutlastdigit2 / 2)
+  const lastDigit4 = numwithoutlastdigit3 % 2
+  const numwithoutlastdigit4 = Math.floor(numwithoutlastdigit3 / 2)
+  const lastDigit5 = numwithoutlastdigit4 % 2
+  const numwithoutlastdigit5 = Math.floor(numwithoutlastdigit4 / 2)
+  const lastDigit6 = numwithoutlastdigit5 % 2
+  const numwithoutlastdigit6 = Math.floor(numwithoutlastdigit5 / 2)
+
+  
   createCanvas(1000, 600);
   background("black");
 
@@ -9,13 +22,12 @@ function setup() {
   strokeWeight(2);
   textAlign(CENTER, CENTER);
   textSize(30);
-  text(`${Math.floor(num / 100000) % 10}`, width / 14, height / 3, width / 9, height / 3);
-
-  text(`${Math.floor(num / 100000) % 10}`, width / 5, height / 3, width / 9, height / 3);
-  text(`${Math.floor(num / 100000) % 10}`, width / 3, height / 3, width / 9, height / 3);
-  text(`${Math.floor(num / 100000) % 10}`, width / 2.15, height / 3, width / 9, height / 3);
-  text(`${Math.floor(num / 100000) % 10} `, width / 1.68, height / 3, width / 9, height / 3);
-  text(`${Math.floor(num) % 10}`, width / 1.38, height / 3, width / 9, height / 3);
+  text(`${lastDigit1}`, width / 14, height / 3, width / 9, height / 3);
+  text(`${lastDigit1}`, width / 5, height / 3, width / 9, height / 3);
+  text(`${lastDigit3}`, width / 3, height / 3, width / 9, height / 3);
+  text(`${lastDigit4}`, width / 2.15, height / 3, width / 9, height / 3);
+  text(`${lastDigit5} `, width / 1.68, height / 3, width / 9, height / 3);
+  text(`${lastDigit6}`, width / 1.38, height / 3, width / 9, height / 3);
 
   textAlign(CENTER, CENTER);
   fill("yellow");
