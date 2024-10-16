abe55eda407645d0201fea9e4836decd31bfb64c
Rainer Stropek <rainer@timecockpit.com>
2024-10-16 14:35:36 +0200

    Saved in TypeScript Playground at 2024-10-16T12:35:36.588Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -15,16 +15,16 @@ function setup() {
   const numwithoutlastdigit6 = Math.floor(numwithoutlastdigit5 / 2)
 
 
-  createCanvas(1000, 600);
+  createCanvas(600, 600);
   background("black");
 
   fill("yellow");
   strokeWeight(2);
   textAlign(CENTER, CENTER);
   textSize(30);
-  text(`${lastDigit6}`, width / 3, height / 3, width / 9, height / 3);
-  text(`${lastDigit5}`, width / 6, height / 3, width / 9, height / 3);
-  text(`${lastDigit4}`, width / 2, height / 3, width / 9, height / 3);
+  text(`${lastDigit6}`, width / 3, height / 3, width / 7, height / 3);
+  text(`${lastDigit5}`, width / 6, height / 3, width / 7, height / 3);
+  text(`${lastDigit4}`, width / 2, height / 3, width / 7, height / 3);
 
   textAlign(CENTER, CENTER);
   fill("yellow");
@@ -34,8 +34,8 @@ function setup() {
   stroke("yellow");
   noFill();
   strokeWeight(2);
-  rect(width / 3, height / 3, width / 9, height / 3)
-  rect(width / 6, height / 3, width / 9, height / 3)
-  rect(width / 2, height / 3, width / 9, height / 3)
+  rect(width / 8, height / 3, width / 10, height / 4);
+  rect(width / 8 * 2.5, height / 3, width / 10, height / 4);
+  rect(width / 8 * 4, height / 3, width / 10, height / 4);
 
 }
