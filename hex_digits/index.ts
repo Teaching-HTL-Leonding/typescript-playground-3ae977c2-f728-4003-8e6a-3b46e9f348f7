bea5436805a0fb39e181072f35ea7f3e2856ae07
Rainer Stropek <rainer@timecockpit.com>
2024-10-16 14:10:46 +0200

    Saved in TypeScript Playground at 2024-10-16T12:10:45.976Z

--- hex_digits/index.ts
+++ hex_digits/index.ts
@@ -1,6 +1,6 @@
 function setup() {
 
-  const num = Math.floor(random(0, 64));
+  const num = Math.floor(random(0, 151515));
   const lastDigit1 = num % 2
   const numwithoutlastdigit1 = Math.floor(num / 2)
   const lastDigit2 = numwithoutlastdigit1 % 2
@@ -14,7 +14,7 @@ function setup() {
   const lastDigit6 = numwithoutlastdigit5 % 2
   const numwithoutlastdigit6 = Math.floor(numwithoutlastdigit5 / 2)
 
-  
+
   createCanvas(1000, 600);
   background("black");
 
@@ -22,12 +22,9 @@ function setup() {
   strokeWeight(2);
   textAlign(CENTER, CENTER);
   textSize(30);
-  text(`${lastDigit6}`, width / 14, height / 3, width / 9, height / 3);
-  text(`${lastDigit5}`, width / 5, height / 3, width / 9, height / 3);
-  text(`${lastDigit4}`, width / 3, height / 3, width / 9, height / 3);
-  text(`${lastDigit3}`, width / 2.15, height / 3, width / 9, height / 3);
-  text(`${lastDigit2} `, width / 1.68, height / 3, width / 9, height / 3);
-  text(`${lastDigit1}`, width / 1.38, height / 3, width / 9, height / 3);
+  text(`${lastDigit6}`, width / 3, height / 3, width / 9, height / 3);
+  text(`${lastDigit5}`, width / 6, height / 3, width / 9, height / 3);
+  text(`${lastDigit4}`, width / 2, height / 3, width / 9, height / 3);
 
   textAlign(CENTER, CENTER);
   fill("yellow");
@@ -37,11 +34,8 @@ function setup() {
   stroke("yellow");
   noFill();
   strokeWeight(2);
-  rect(width / 14, height / 3, width / 9, height / 3);
-  rect(width / 5, height / 3, width / 9, height / 3);
-  rect(width / 3, height / 3, width / 9, height / 3);
-  rect(width / 2.15, height / 3, width / 9, height / 3);
-  rect(width / 1.68, height / 3, width / 9, height / 3);
-  rect(width / 1.38, height / 3, width / 9, height / 3);
-  
+  rect(width / 3, height / 3, width / 9, height / 3)
+  rect(width / 6, height / 3, width / 9, height / 3)
+  rect(width / 2, height / 3, width / 9, height / 3)
+
 }
