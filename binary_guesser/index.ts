949710e9c4752c35c48e5504606e2da5e1ff45b2
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 10:12:32 +0200

    Saved in TypeScript Playground at 2024-10-17T08:12:32.361Z

---
+++ binary_guesser/index.ts
@@ -0,0 +1,34 @@
+let num = 0;
+const digit1 = 0
+
+
+function setup() {
+  num = Math.floor(random(0, 16));
+
+  createCanvas(600, 400);
+  background("black");
+
+  fill("yellow")
+  textSize(40);
+  text(num, width / 2, height / 5);
+  text(0, width / 7, height / 2, width / 6, height / 2);
+  text(0,  width / 7 * 2.5, height / 2, width / 6, height / 2);
+  text(0, width / 7 * 4, height / 2, width / 6, height / 2);
+  text(0, width / 7 * 5.5, height / 2, width / 6, height / 2)
+
+  stroke("yellow")
+  strokeWeight(2)
+  noFill()
+  rect(width / 8, height / 3, width / 7, height / 3);
+  rect(width / 8 * 2.5, height / 3, width / 7, height / 3);
+  rect(width / 8 * 4, height / 3, width / 7, height / 3);
+  rect(width / 8 * 5.5, height / 3, width / 7, height / 3)
+
+
+
+}
+
+function mouseClicked() {
+if(mouseX < width / 7 && mouseY < height / 2)
+}
+ 
