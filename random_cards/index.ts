603460ec10f3e1e65202875df5037f50335b424f
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 09:33:51 +0200

    Saved in TypeScript Playground at 2024-10-24T07:33:51.104Z

--- random_cards/index.ts
+++ random_cards/index.ts
@@ -20,18 +20,10 @@ function preload() {
   const cardIx = Math.floor(random(1, 14));
   switch (cardIx) {
     case 1: rank = "A"; break
-    case 2: rank = "2"; break
-    case 3: rank = "3"; break
-    case 4: rank = "4"; break
-    case 5: rank = "5"; break
-    case 6: rank = "6"; break
-    case 7: rank = "7"; break
-    case 8: rank = "8"; break
-    case 9: rank = "9"; break
-    case 10: rank = "10"; break
     case 11: rank = "J"; break
     case 12: rank = "Q"; break
     case 13: rank = "K"; break
+    default: rank= `${cardIx}`; break
 
     
   }
