7f3ff1dad8c57a252de2fae06545894bc2cfff35
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 10:40:32 +0200

    Saved in TypeScript Playground at 2024-10-24T08:40:32.144Z

--- calculator_if/index.ts
+++ calculator_if/index.ts
@@ -5,7 +5,7 @@ let lineHeight: number = 0;   // Height of a line of the responsive layout
 let cellWidth: number = 0;    // Width of a cell of the responsive layout
 
 function setup() {
-    createCanvas(400, 600);
+    createCanvas(300, 500);
     lineHeight = height / 5;
     cellWidth = width / 3;
 }
@@ -20,11 +20,23 @@ function draw() {
     stroke("black");
     fill("black");
     strokeWeight(2);
-    textSize(50);
+    textSize(55);
+    textAlign(CENTER, CENTER)
 
-    line()
+    line(0, lineHeight, width, lineHeight);
+    text('9', width / 3 * 2 + cellWidth / 2, lineHeight + lineHeight / 2);
+    text('8', width / 3 + cellWidth / 2, lineHeight + lineHeight / 2);
+    text('7', cellWidth / 2, lineHeight + lineHeight / 2);
+    text('6', width / 3 * 2 + cellWidth / 2, lineHeight * 2.5)
+    text('5', width / 3 + cellWidth / 2, lineHeight * 2.5)
+    text('4', cellWidth / 2, lineHeight * 2.5)
+    text('3', width / 3 * 2 + cellWidth / 2, lineHeight * 3.5)
+    text('2', width / 3  + cellWidth / 2, lineHeight * 3.5)
+    text('1', cellWidth / 2, lineHeight * 3.5 )
+    text('0', cellWidth , lineHeight * 4.5)
+    text('C', width / 3 * 2 + cellWidth / 2, lineHeight * 4.5)
 
-    
+
 
 
 }
