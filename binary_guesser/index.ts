3ce6500d04b067bc03914c79097a93d249a493d6
Rainer Stropek <rainer@timecockpit.com>
2024-11-05 19:02:09 +0100

    Saved in TypeScript Playground at 2024-11-05T18:02:08.877Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -5,98 +5,102 @@ let guessedDigit4 = 0;
 let num = 0;
 
 function setup() {
-  num = Math.floor(random(1, 16));
+  num = Math.floor(random(0, 16));
 
-  createCanvas(400, 300);
-  background("black");
+  createCanvas(600, 400)
+  background("black")
 
-  
-  noFill();
-  stroke("yellow");
-  rect(50 + 0 * 75, 100, 50, 75);
-  rect(50 + 1 * 75, 100, 50, 75);
-  rect(50 + 2 * 75, 100, 50, 75);
-  rect(50 + 3 * 75, 100, 50, 75);
+  noFill()
+  stroke("Yellow")
+  rect(width / 6, height / 3, width / 9, height / 3)
+  rect(width / 6 * 2, height / 3, width / 9, height / 3)
+  rect(width / 6 * 3, height / 3, width / 9, height / 3)
+  rect(width / 6 * 4, height / 3, width / 9, height / 3)
 
-  
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  textSize(30);
-  text(`${num} in binary?`, width / 2, height / 6);
+  textAlign(CENTER, CENTER)
+  fill("Yellow")
+  textSize(25)
+  text(guessedDigit1, width / 6, height / 3, width / 9, height / 3)
+  text(guessedDigit2, width / 6 * 2, height / 3, width / 9, height / 3)
+  text(guessedDigit3, width / 6 * 3, height / 3, width / 9, height / 3)
+  text(guessedDigit4, width / 6 * 4, height / 3, width / 9, height / 3)
 
-  text(0, 75 + 0 * 75, 100 + 75 / 2);
-  text(0, 75 + 1 * 75, 100 + 75 / 2);
-  text(0, 75 + 2 * 75, 100 + 75 / 2);
-  text(0, 75 + 3 * 75, 100 + 75 / 2);
+  textAlign(CENTER, CENTER)
+  fill("Yellow")
+  noStroke()
+  textSize(25)
+  text(`${num} in binary?`, width / 2, height / 6)
+
+
 }
 
 function mouseClicked() {
-  if (mouseY >= 100 && mouseY <= 175) {
 
-    if (mouseX >= 50 + 0 * 75 && mouseX <= 50 + 0 * 75 + 50) {
-      if (guessedDigit4 === 1) {
-        guessedDigit4 = 0;
-      } else {
-        guessedDigit4 = 1;
-      }
+
+  if (mouseX > width / 6 * 4 && mouseX < width / 9) {
+    if (guessedDigit4 === 1) {
+      guessedDigit4 = 0;
+    } else {
+      guessedDigit4 = 1;
     }
+  }
 
-    if (mouseX >= 50 + 1 * 75 && mouseX <= 50 + 1 * 75 + 50) {
-      if (guessedDigit3 === 1) {
-        guessedDigit3 = 0;
-      } else {
-        guessedDigit3 = 1;
-      }
+  if (mouseX > width / 6 * 3 && mouseX < width / 9) {
+    if (guessedDigit3 === 1) {
+      guessedDigit3 = 0;
+    } else {
+      guessedDigit3 = 1;
     }
+  }
 
-    if (mouseX >= 50 + 2 * 75 && mouseX <= 50 + 2 * 75 + 50) {
-      if (guessedDigit2 === 1) {
-        guessedDigit2 = 0;
-      } else {
-        guessedDigit2 = 1;
-      }
+  if (mouseX > width / 6 * 2 && mouseX < width / 9) {
+    if (guessedDigit2 === 1) {
+      guessedDigit2 = 0;
+    } else {
+      guessedDigit2 = 1;
     }
+  }
 
-    if (mouseX >= 50 + 3 * 75 && mouseX <= 50 + 3 * 75 + 50) {
-      if (guessedDigit1 === 1) {
-        guessedDigit1 = 0;
-      } else {
-        guessedDigit1 = 1;
-      }
-    }
-
-    // Build new guessed number
-    let guessedNumber = guessedDigit4 * Math.pow(2, 3);
-    guessedNumber += guessedDigit3 * Math.pow(2, 2);
-    guessedNumber += guessedDigit2 * Math.pow(2, 1);
-    guessedNumber += guessedDigit1 * Math.pow(2, 0);
-
-    
-    let color = "yellow";
-    if (guessedNumber === num) {
-      
-      color = "lime";
-    }
-    fill("black");
-    stroke(color);
-
-    rect(50 + 0 * 75, 100, 50, 75);
-    rect(50 + 1 * 75, 100, 50, 75);
-    rect(50 + 2 * 75, 100, 50, 75);
-    rect(50 + 3 * 75, 100, 50, 75);
-
-    textAlign(CENTER, CENTER);
-    fill(color);
-    noStroke();
-    textSize(30);
-    text(guessedDigit4, 75 + 0 * 75, 100 + 75 / 2);
-    text(guessedDigit3, 75 + 1 * 75, 100 + 75 / 2);
-    text(guessedDigit2, 75 + 2 * 75, 100 + 75 / 2);
-    text(guessedDigit1, 75 + 3 * 75, 100 + 75 / 2);
-
-    if (guessedNumber === num) {
-      text("Correct!", width / 2, 220);
-    } 
-   }
 }
+if (mouseX > width / 6 && height / 9) {
+  if (guessedDigit1 === 1) {
+    guessedDigit1 = 0;
+  } else {
+    guessedDigit1 = 1;
+  }
+}
+
+let guessedNumber = guessedDigit4 * Math.pow(2, 3);
+guessedNumber += guessedDigit3 * Math.pow(2, 2);
+guessedNumber += guessedDigit2 * Math.pow(2, 1);
+guessedNumber += guessedDigit1 * Math.pow(2, 0);
+
+
+
+if (guessedNumber === num) {
+
+  
+}
+fill("black");
+stroke("yellow");
+
+rect(width / 6, height / 3, width / 9, height / 3)
+rect(width / 6 * 2, height / 3, width / 9, height / 3)
+rect(width / 6 * 3, height / 3, width / 9, height / 3)
+rect(width / 6 * 4, height / 3, width / 9, height / 3)
+
+textAlign(CENTER, CENTER);
+fill(color);
+noStroke();
+textSize(30);
+text(guessedDigit1, width / 6, height / 3, width / 9, height / 3)
+text(guessedDigit2, width / 6 * 2, height / 3, width / 9, height / 3)
+text(guessedDigit3, width / 6 * 3, height / 3, width / 9, height / 3)
+text(guessedDigit4, width / 6 * 4, height / 3, width / 9, height / 3)
+
+if (guessedNumber === num) {
+  text("Correct!", width / 2, 220);
+}
+
+
+
