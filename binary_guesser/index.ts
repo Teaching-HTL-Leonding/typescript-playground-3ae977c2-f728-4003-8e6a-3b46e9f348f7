84969dc4f8c21cfdf1db66df09f35b6c4db047c0
Rainer Stropek <rainer@timecockpit.com>
2024-11-06 17:52:10 +0100

    Saved in TypeScript Playground at 2024-11-06T16:52:10.331Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -12,95 +12,98 @@ function setup() {
 
   noFill()
   stroke("Yellow")
-  rect(width / 6, height / 3, width / 9, height / 3)
-  rect(width / 6 * 2, height / 3, width / 9, height / 3)
-  rect(width / 6 * 3, height / 3, width / 9, height / 3)
-  rect(width / 6 * 4, height / 3, width / 9, height / 3)
+  rect(100, 100, 600 / 9, 400 / 3)
+  rect(200, 100, 600 / 9, 400 / 3)
+  rect(300, 100, 600 / 9, 400 / 3)
+  rect(400, 100, 600 / 9, 400 / 3)
 
   textAlign(CENTER, CENTER)
   fill("Yellow")
-  textSize(25)
-  text(guessedDigit1, width / 6, height / 3, width / 9, height / 3)
-  text(guessedDigit2, width / 6 * 2, height / 3, width / 9, height / 3)
-  text(guessedDigit3, width / 6 * 3, height / 3, width / 9, height / 3)
-  text(guessedDigit4, width / 6 * 4, height / 3, width / 9, height / 3)
+  textSize(30)
+  text(guessedDigit4, width / 6, height / 4, width / 9, height / 3)
+  text(guessedDigit3, width / 6 * 2, height / 4, width / 9, height / 3)
+  text(guessedDigit2, width / 6 * 3, height / 4, width / 9, height / 3)
+  text(guessedDigit1, width / 6 * 4, height / 4, width / 9, height / 3)
 
   textAlign(CENTER, CENTER)
   fill("Yellow")
   noStroke()
-  textSize(25)
+  textSize(30)
   text(`${num} in binary?`, width / 2, height / 6)
 
 
 }
 
+
 function mouseClicked() {
+  if (mouseY >= height / 4 && mouseY <= height / 3) {
 
+    if (mouseX >= 600 / 9 && mouseX <= 600 / 6) {
+      if (guessedDigit4 === 1) {
+        guessedDigit4 = 0
+      } else {
+        guessedDigit4 = 1
+      }
+    }
 
-  if (mouseX > width / 6 * 4 && mouseX < width / 9) {
-    if (guessedDigit4 === 1) {
-      guessedDigit4 = 0;
-    } else {
-      guessedDigit4 = 1;
+    if (mouseX >= 600 / 9 && mouseX <= 600 / 6 * 2) {
+      if (guessedDigit3 === 1) {
+        guessedDigit3 = 0
+      } else {
+        guessedDigit3 = 1
+      }
+    }
+
+    if (mouseX >= 600 / 9 && mouseX <= 600 / 6 * 3) {
+      if (guessedDigit2 === 1) {
+        guessedDigit2 = 0
+      } else {
+        guessedDigit2 = 1
+      }
+    }
+
+    if (mouseX >= 600 / 9 && mouseX <= 600 / 6 * 4) {
+      if (guessedDigit1 === 1) {
+        guessedDigit1 = 0
+      } else {
+        guessedDigit1 = 1
+      }
     }
   }
 
-  if (mouseX > width / 6 * 3 && mouseX < width / 9) {
-    if (guessedDigit3 === 1) {
-      guessedDigit3 = 0;
-    } else {
-      guessedDigit3 = 1;
-    }
+
+
+  let guessedNumber = guessedDigit4 * Math.pow(2, 3);
+  guessedNumber += guessedDigit3 * Math.pow(2, 2);
+  guessedNumber += guessedDigit2 * Math.pow(2, 1);
+  guessedNumber += guessedDigit1 * Math.pow(2, 0);
+
+
+  let color = "yellow";
+  if (guessedNumber === num) {
+
+    color = "lime";
   }
+  fill("black");
+  stroke(color);
 
-  if (mouseX > width / 6 * 2 && mouseX < width / 9) {
-    if (guessedDigit2 === 1) {
-      guessedDigit2 = 0;
-    } else {
-      guessedDigit2 = 1;
-    }
+  rect(width / 6, height / 4, width / 9, height / 3)
+  rect(width / 6 * 2, height / 4, width / 9, height / 3)
+  rect(width / 6 * 3, height / 4, width / 9, height / 3)
+  rect(width / 6 * 4, height / 4, width / 9, height / 3)
+
+  textAlign(CENTER, CENTER);
+  fill(color);
+  noStroke();
+  textSize(30);
+  text(guessedDigit4, width / 6, height / 4, width / 9, height / 3)
+  text(guessedDigit3, width / 6 * 2, height / 4, width / 9, height / 3)
+  text(guessedDigit2, width / 6 * 3, height / 4, width / 9, height / 3)
+  text(guessedDigit1, width / 6 * 4, height / 4, width / 9, height / 3)
+
+  if (guessedNumber === num) {
+    text("Correct!", width / 2, 220);
   }
-
 }
-if (mouseX > width / 6 && height / 9) {
-  if (guessedDigit1 === 1) {
-    guessedDigit1 = 0;
-  } else {
-    guessedDigit1 = 1;
-  }
-}
-
-let guessedNumber = guessedDigit4 * Math.pow(2, 3);
-guessedNumber += guessedDigit3 * Math.pow(2, 2);
-guessedNumber += guessedDigit2 * Math.pow(2, 1);
-guessedNumber += guessedDigit1 * Math.pow(2, 0);
-
-
-
-if (guessedNumber === num) {
-
-  
-}
-fill("black");
-stroke("yellow");
-
-rect(width / 6, height / 3, width / 9, height / 3)
-rect(width / 6 * 2, height / 3, width / 9, height / 3)
-rect(width / 6 * 3, height / 3, width / 9, height / 3)
-rect(width / 6 * 4, height / 3, width / 9, height / 3)
-
-textAlign(CENTER, CENTER);
-fill(color);
-noStroke();
-textSize(30);
-text(guessedDigit1, width / 6, height / 3, width / 9, height / 3)
-text(guessedDigit2, width / 6 * 2, height / 3, width / 9, height / 3)
-text(guessedDigit3, width / 6 * 3, height / 3, width / 9, height / 3)
-text(guessedDigit4, width / 6 * 4, height / 3, width / 9, height / 3)
-
-if (guessedNumber === num) {
-  text("Correct!", width / 2, 220);
-}
-
 
 
