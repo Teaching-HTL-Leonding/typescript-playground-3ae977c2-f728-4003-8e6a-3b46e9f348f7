fc1f69f184ab54b3491ce991267c8d06c4ea8663
Rainer Stropek <rainer@timecockpit.com>
2024-11-13 11:44:07 +0100

    Saved in TypeScript Playground at 2024-11-13T10:44:07.006Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,22 +1,69 @@
+
 function setup() {
-    createCanvas(250, 250)
-    background("black")
+    createCanvas(400, 400);
+    background("black");
 
-    strokeWeight(1)
-    colorMode(RGB)
-    strokeCap(SQUARE)
+    let firstrandumnum = Math.floor(random(0, 100));
+    let secondrandomnum = Math.floor(random(0, 100));
+    let thirdrandomnum = Math.floor(random(0, 100));
+    let num4 = Math.floor(random(0, 100));
+    let num5 = Math.floor(random(0, 100));
+    let operator = Math.floor(random(1, 4));
+    let correctAnswer = 0
+    let placeofnum = Math.floor(random(1, 4));
 
-    let y = 225
-    while (y >= 25) {
-        stroke(y, 100, 100)
-        line(25, y, y, 225)
-        line(y, 25, 225, y)
+    let message: string
 
-        y -= 0.5
+
+    fill("yellow");
+
+    textAlign(CENTER, LEFT);
+    textSize(55);
+    text(`${firstrandumnum}`, width / 6, height / 2);
+    text(`${secondrandomnum}`, width / 2, height / 2);
+    text(`${thirdrandomnum}`, width - 35, height / 2);
+
+    textAlign(CENTER, RIGHT);
+    textSize(55);
+
+    text(`${num4}`, width / 4, height / 5);
+    text(`${num5}`, width / 4 * 3, height / 5);
+
+    if (operator === 1) {
+        text('+', width / 2, height / 6)
+    } else if (operator === 2) {
+        text('-', width / 2, height / 6)
+    } else {
+        text('*', width / 2, height / 6)
+    }
+
+    if (operator ===1) { 
+        if (correctAnswer === 1)
+        firstrandumnum = (`${num4 + num5}`)
+        
     }
 
 }
 
 
-function draw() {
+
+
+// mouseClicked function checks if the user clicked on the correct answer
+function mouseClicked() {
+
+}
+
+// mouseMoved function displays mouse coordinates as feedback
+function mouseMoved() {
+    // This is a HELPER FUNCTION. It should make finding coordinates
+    // easier for you. You DO NOT NEED to change this method!
+
+    fill("black");
+    noStroke();
+    rect(0, height - 20, width, height); // Draws a rectangle to clear previous coordinates
+
+    fill("white");
+    textSize(10);
+    textAlign(LEFT, BOTTOM); // Aligns coordinates text to bottom-left
+    text(`${mouseX}/${mouseY}`, 5, height - 5); // Displays current mouse coordinates
 }
