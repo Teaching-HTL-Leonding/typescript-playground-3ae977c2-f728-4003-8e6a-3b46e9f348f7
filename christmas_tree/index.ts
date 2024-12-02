2b08669a64c74ead40a05261fc163b08a3197294
Rainer Stropek <rainer@timecockpit.com>
2024-12-02 19:10:58 +0100

    Saved in TypeScript Playground at 2024-12-02T18:10:57.777Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -6,12 +6,17 @@ const HEIGHT_TO_WIDTH_RATIO = 0.5;
 const NUMBER_OF_LAYERS = 4;
 const TRUNK_WIDTH = 50;
 const TRUNK_HEIGHT = 50;
-const SIZE = 500
 
 function setup() {
-  createCanvas(SIZE, SIZE);
+  createCanvas(500, 500);
   background("black");
   fill("green");
   noStroke();
 
+  translate(width / 2, TOP_MARGIN)
+  let trianglewidth = INITIAL_TRIANGLE_BASE_WIDTH
+  for(let i = 0; i < NUMBER_OF_LAYERS; i ++) {
+    triangle(- trianglewidth / 2, trianglewidth / 2, 0, 0, trianglewidth / 2, trianglewidth / 2)
+    translate(0, trianglewidth / 2)
+  }
 }
