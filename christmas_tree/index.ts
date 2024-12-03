39cb1d3a083dc5c4a344f8b5618bc859a7edc38b
Rainer Stropek <rainer@timecockpit.com>
2024-12-03 16:46:15 +0100

    Saved in TypeScript Playground at 2024-12-03T15:46:15.219Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -9,28 +9,24 @@ const TRUNK_HEIGHT = 50;
 
 function setup() {
   colorMode(HSB)
-  let color1 = 0
+  let colorofcircle = 0
   createCanvas(500, 500);
   background("black");
-  fill("green");
-  noStroke();
-
+  noStroke(); 77
   let trianglewidth = INITIAL_TRIANGLE_BASE_WIDTH
 
   translate(width / 2, TOP_MARGIN)
   for (let i = 0; i < NUMBER_OF_LAYERS; i++) {
-    fill("green")
-    triangle(- trianglewidth / 2, trianglewidth / 2, 0, 0, trianglewidth / 2, trianglewidth / 2)
-    fill(color1, 75, 75)
-    circle(- trianglewidth / 2, trianglewidth / 2 +15, SPHERE_DIAMETER)
-    circle( trianglewidth / 2, trianglewidth / 2 +15, SPHERE_DIAMETER)
+    fill("green");
+    triangle(-trianglewidth / 2, trianglewidth / 2, 0, 0, trianglewidth / 2, trianglewidth / 2)
+
+    fill(colorofcircle, 80, 80)
+    circle(-trianglewidth / 2, trianglewidth / 2 + SPHERE_DIAMETER / 2, SPHERE_DIAMETER)
+    circle(trianglewidth / 2, trianglewidth / 2 + SPHERE_DIAMETER / 2, SPHERE_DIAMETER)
 
     translate(0, trianglewidth / 4)
     trianglewidth *= 1.5
-    color1 += 60
+    colorofcircle += 60
   }
 
-  fill("brown")
-  translate(0, trianglewidth / 1.5 / 2 / 2)
-  rect(- TRUNK_WIDTH / 2, 0, TRUNK_WIDTH, TRUNK_HEIGHT)
 }
