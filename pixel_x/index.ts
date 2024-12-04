16d07e04be8945b2a89b1360342cd4a46c806e34
Rainer Stropek <rainer@timecockpit.com>
2024-12-04 10:43:45 +0100

    Saved in TypeScript Playground at 2024-12-04T09:43:45.548Z

--- pixel_x/index.ts
+++ pixel_x/index.ts
@@ -1,18 +1,26 @@
 function setup() {
-    const SIZE = 425; // Canvas size
-    const GRID = 25;  // Size of the squares
+    const SIZE = 425;
+    const GRID = 25;
 
     createCanvas(SIZE, SIZE);
     background("black");
 
     noStroke();
+    fill("yellow")
 
-    let i = GRID
-    while (i < SIZE - GRID) {
-        fill("yellow")
-        rect(i, i, GRID, GRID)
-        fill("lightgreen")
-        rect(i, SIZE - GRID - i, GRID, GRID)
-        i += GRID
+    translate(GRID, GRID)
+    for (let i = 0; i < 15; i++) {
+        rect(0, 0, GRID, GRID)
+        translate(GRID, GRID)
     }
+
+    resetMatrix()
+    fill("lime")
+    translate(GRID, SIZE - GRID - GRID)
+    for (let i = 0; i < 15; i++) {
+        rect(0, 0, GRID, GRID)
+        translate(GRID, -GRID)
+    }
+
+
 }
