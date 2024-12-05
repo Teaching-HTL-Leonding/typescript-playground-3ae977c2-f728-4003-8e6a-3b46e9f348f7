cd0c78aa2bd20c32abe50b080a8bbacf1d5bbd12
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 09:16:34 +0100

    Saved in TypeScript Playground at 2024-12-05T08:16:33.938Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -11,38 +11,36 @@ function setup() {
 }
 
 function mouseMoved() {
-    colorMode(HSB)
-    let colorOfLines = 0
-    let sizeofpatter = 10
+    colorMode(HSB);
+    let colorOfLines = 0;
+    let sizeofpatter = 10;
     resetMatrix();
     background("black");
     strokeWeight(2);
     noFill();
 
     for (let i = 0; i < 601; i++) {
-        push()
+        push();
         for (let j = 0; j < 601; j++) {
-            stroke(colorOfLines, 100, 100)
-            line(0, 0, sizeofpatter, 5)
-            line(10, 4.6, 0, sizeofpatter)
-            translate(0, sizeofpatter)
+            stroke(colorOfLines, 100, 100);
+            line(0, 0, sizeofpatter, 5);
+            line(10, 4.6, 0, sizeofpatter);
+            translate(0, sizeofpatter);
         }
-        pop()
-        translate(10, 0)
-        colorOfLines += 6
+        pop();
+        translate(10, 0);
+        colorOfLines += 6;
     }
 
-    resetMatrix()
-    fill("black")
-    noStroke()
-    rect(0, 260, 601, 50)
+    resetMatrix();
+    fill("black");
+    noStroke();
+    rect(0, 260, 601, 50);
 
-    textAlign(CENTER, LEFT)
-    textSize(15)
-    fill("white")
-    text(`${mouseX} of 601 =`, 50, 280)
-    text(`${(mouseX * 100 / 601) + 100}`, 180, 280)
-
-
-
+    const inpercent = Math.floor(mouseX * 100 / 601) + 100;
+    textAlign(CENTER, LEFT);
+    textSize(15);
+    fill("white");
+    text(`${mouseX} of 601 =`, 50, 280);
+    text(`${inpercent} %`, 120, 280);
 }
