ad5d3a56a88d23fd77fe6eb92ac45b593fdb3260
Rainer Stropek <rainer@timecockpit.com>
2025-01-01 15:30:24 +0100

    Saved in TypeScript Playground at 2025-01-01T14:30:23.688Z

--- word_swirrel_level_3/index.ts
+++ word_swirrel_level_3/index.ts
@@ -1,48 +1,59 @@
-const WIDTH = 500;
-const HEIGHT = 300;
-const MARGIN = 50;
+//           +------------- Syntax for datatype "array of strings"
+//           |          +-- Elements of array are enclosed with []
+//           V          V
 const WORDS: string[] = [
-"apple",
-"banana",
-"cherry",
-"oranges",
-"grapes",
-"berry",
-"house",
-"peach"
-]
-
-let wordToGuess = random(WORDS);
-
-function setup() {
-  createCanvas(WIDTH, HEIGHT);
-  background("black");
-  textAlign(CENTER, CENTER);
-  colorMode(HSB);
-  angleMode(DEGREES);
-
-  for (let i = 0; i < wordToGuess.length; i++) {
-    // Get random position, font size, text color, and angle
-    const x = random(MARGIN, WIDTH - MARGIN);
-    const y = random(MARGIN, HEIGHT - MARGIN);
-    const charSize = random(50, 150);
-    const textColor = random(0, 360);
-    const angle = random(-90, 90);
-
-    push();
-    translate(x, y);
-    // This is NEW! We can easily rotate text.
-    // NOTE: _rotate_ rotates around the origin (0, 0). Therefore it is important
-    //       that you _translate_ to the position you want to draw the text at.
-    //       We covered _translate_ in previous examples.
-    rotate(angle);
-    fill(textColor, 100, 100);
-    textSize(charSize);
-    text(wordToGuess[i], 0, 0);
-    pop();
+    "apple",
+    "banana",
+    "cherry",
+    "orange",
+    "grapes",
+    "melon",
+    "peach",
+    "plum",
+    "berry",
+    "water",
+    "lemon",
+    "pizza",
+    "cloud",
+    "bread",
+    "cheese",
+ 
+  ];
+  
+  const WIDTH = 500;
+  const HEIGHT = 500;
+  const MARGIN = 75;
+  
+  let wordToGuess: string;
+  
+  function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    background("black");
+    textAlign(CENTER, CENTER);
+    colorMode(HSB);
+    angleMode(DEGREES);
+  
+    // Use random() to get a random word from the WORDS array.
+    // Every time you run the program, a different word will be selected.
+    wordToGuess = random(WORDS);
+  
+    for(let i = 0; i < wordToGuess.length; i++) {
+      const x = random(MARGIN, WIDTH - MARGIN);
+      const y = random(MARGIN, HEIGHT - MARGIN);
+      const charSize = random(50, 200);
+      const textColor = random(0, 360);
+      const angle = random (-90, 90);
+  
+      push();
+      translate(x, y);
+      rotate(angle);
+      fill(textColor, 100, 100);
+      textSize(charSize);
+      text(wordToGuess[i], 0, 0);
+      pop();
+    }
   }
-}
-
+  
 function guess(textInput: string) {
   fill("white");
 
