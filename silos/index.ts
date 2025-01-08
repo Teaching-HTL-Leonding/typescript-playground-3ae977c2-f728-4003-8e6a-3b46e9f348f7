d42ef1d20b6807a713a257bc64c4bae35195b735
Rainer Stropek <rainer@timecockpit.com>
2025-01-08 19:28:50 +0100

    Saved in TypeScript Playground at 2025-01-08T18:28:49.999Z

--- silos/index.ts
+++ silos/index.ts
@@ -1,13 +1,10 @@
-// Set the initial fill levels for the silos. The fill levels are separated by commas.
 const INITIAL_FILL = "3,7,8,3,10,2"
 
-const SILO_MAX = 10; // Maximum fill for each silo
-const CRICITAL_FILL = 8; // Critical fill level for each silo
+const SILO_MAX = 10;
+const CRICITAL_FILL = 8;
 
-// Store the fill values for the silos. Will be a value between 0 and SILO_MAX.
 const silos: number[] = [];
 
-// Constants for the layout
 const CANVAS_HEIGHT = 500;
 const CANVAS_WIDTH = 480;
 
@@ -20,38 +17,48 @@ const BUTTON_SIZE = 20; // Size of the buttons ("add" and "remove")
 const BUTTON_GAP = 15; // Gap between the bottom of the buttons and the top of the silos
 const BUTTON_TOP = SILOS_TOP - BUTTON_GAP - BUTTON_SIZE; // Y-coordinate of the top of the buttons
 
-// <<< Add your arrays here
+
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 
-  // <<< Add your code here
+  let num = 0;
+  for (let i = 0; i < INITIAL_FILL.length; i++) {
+    if (INITIAL_FILL[i] === ",") {
+      silos.push(num);
+      num = 0;
+    } else {
+      num = num * 10 + parseInt(INITIAL_FILL[i].trim());
+    }
+  }
+
+
+  silos.push(num);
 }
 
 function draw() {
-  background("black")
+  background("black");
 
+  textSize(25)
+  fill("yellow")
+  noStroke()
+  for (let i = 0; i < INITIAL_FILL.length; i++) {
+    text(silos[i], 45 + 75 * i, width - 50)
+  }
 
   stroke("yellow")
   strokeWeight(3)
-  translate(SILO_GAP + 10, 80)
-  for (let i = 0; i <= CANVAS_WIDTH - SILO_GAP ; i+= 35) {
-    line(i, 0, i, SILO_HEIGHT - SILO_GAP)
-    
+
+  translate(SILO_GAP, 80)
+
+
+  push()
+  for (let i = 0; i < INITIAL_FILL.length / 2; i++) {
+    line(0, 0, 0, 320)
+    line(50, 0, 50, 320)
+    translate(75, 0)
   }
-
-  fill("yellow")
-  noStroke()
-  textSize(25)
-  textAlign(CENTER, CENTER)
-
-
-  resetMatrix()
-  for (let i = 0; i < INITIAL_FILL.length; i++) {
-    text(INITIAL_FILL[i], 35 * i + SILO_WIDTH, CANVAS_HEIGHT - 100)
-  }
-
-  noFill()
+  pop()
 
 }
 
