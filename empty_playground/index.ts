36f0e14c45d5e143c0bc14a8ab89ba2a8f22a69a
Rainer Stropek <rainer@timecockpit.com>
2025-03-01 17:28:38 +0100

    Saved in TypeScript Playground at 2025-03-01T16:28:38.383Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,23 +1,17 @@
+let stringToParse = "18;4:1";
+let emptyString = "";
+let parsedArray:number[] = [];
+
 function setup() {
-    createCanvas(500, 500)
+    createCanvas(600, 400)
     background("black")
 
-    for (let i = 0; i < 3; i++) {
-        drawCircle("yellow", "red")
+    for(let i = 0; i < stringToParse.length; i++) {
+        if(stringToParse[i] === ";") {
+            emptyString += parseInt(stringToParse[i]);
+        }
     }
 }
 
-function mouseClicked() {
-    drawCircle("lime", "aqua")
-}
-
-function drawCircle(color: string, colorstroke: string) {
-    stroke(colorstroke)
-    strokeWeight(10)
-    fill(color)
-    let x = random(0, 400)
-    let y = random(0, 400)
-    let diameter = random(50, 250)
-    //console.info(x, y, diameter)
-    circle(x, y, diameter)
+function draw() {
 }
