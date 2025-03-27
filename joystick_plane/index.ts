318cbca56a79a04620599babb778a456a987e196
Rainer Stropek <rainer@timecockpit.com>
2025-03-27 09:39:30 +0000

    Saved in TypeScript Playground at 2025-03-27T09:39:30.123Z

---
+++ joystick_plane/index.ts
@@ -0,0 +1,35 @@
+let fighter: p5.Image;
+
+const fighterImageWidth = 475;
+const fighterImageHeight = 724;
+const fighterDisplayHeight = 200;
+const fighterDisplayWidth = fighterDisplayHeight * (fighterImageWidth / fighterImageHeight);
+let fighterPositionX = 0;
+let fighterPositionY = 0;
+
+function preload() {
+  fighter = loadImage("https://cddataexchange.blob.core.windows.net/images/Spaceship.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background("lightblue");
+
+  push();
+  imageMode(CENTER);
+  translate(width / 2 + fighterPositionX, height / 2 + fighterPositionY);
+  image(fighter, 0, 0, fighterDisplayWidth, fighterDisplayHeight);
+  pop();
+}
+
+function mousePressed() {
+}
+
+function mouseDragged() {
+}
+
+function mouseReleased() {
+}
